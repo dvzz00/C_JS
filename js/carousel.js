3b1abfc3d6c0d1e9e2d4e535b347ad2bc31a98d2
Davi Vieira <davifmvieira@gmail.com>
2024-06-17 19:41:53 -0300

    js adicionado

---
+++ js/carousel.js
@@ -0,0 +1,18 @@
+// carousel.js
+
+let currentIndex = 0;
+
+function moveCarousel(direction) {
+    const items = document.querySelectorAll('.carousel-item');
+    const totalItems = items.length;
+
+    items[currentIndex].classList.remove('ativo');
+    currentIndex = (currentIndex + direction + totalItems) % totalItems;
+    items[currentIndex].classList.add('ativo');
+
+    document.querySelector('.carousel-inner').style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+setInterval(() => {
+    moveCarousel(1);
+}, 4000);
